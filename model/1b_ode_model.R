7cf8cb170cd9b051aa35f3d9623d9ced06e4f9d9
jenniesuz <jenniesuz@gmail.com>
2018-04-23 15:29:27 +0100

    eia code update

--- model/1b_ode_model.R
+++ model/1b_ode_model.R
@@ -87,7 +87,7 @@ simPop <- function(init=initial, tseq = times, modFunction=mod, parms = params()
 #****************************************************************
 
 
-persist.delay <- simPop(parms=params(delay=0.01))
+persist.delay <- simPop(parms=params(delay=1.4))
 
 
 
---
+++ model/4_eia_for_ms.R
@@ -0,0 +1,105 @@
+library(rootSolve) 
+library("ggplot2")
+#******************Evolutionary invasion analysis**************************
+
+#***********************COMBINED MODEL ACUTE AND PERSISTENT INFECTION*************************************
+
+#************* mutant virus fitness function without delay in budding or fixed time to apoptosis************
+mutfit <- function(mu_mv=0.1            # mutant virus clearance rate
+                         ,mu_mc = 1/120       # mutant virus cell death rate
+                         ,beta_m = 10^-6      # mutant probability of infecting susceptible cells
+                         ,gamma_m = 2400      # mutant virus yeild at apoptosis
+                         ,lambda_m = 100
+                         ,alpha_m = 1/24      # mutant virus apoptosis rate
+                         ,mu_v=0.1            # resident virus clearance rate  
+                         ,mu_c = 1/120        # resident infected cell death rate 
+                         ,beta = 10^-6        # resident probability of infecting susceptible cells
+                         ,alpha = 1/24        # resident virus apoptosis rate
+                         ,gamma = 2400 
+                         ,lambda = 100){     # resident virus yeild at apoptosis 
+  fit1 <- (-(alpha_m+mu_mc+mu_mv) + sqrt( (alpha_m+mu_mc+mu_mv)^2 - 4*(alpha_m*mu_mv + mu_mv*mu_mc - (beta_m*(gamma_m*alpha_m+lambda_m)*mu_c*(mu_v + alpha) )/((gamma*alpha + lambda)*beta)) ) ) / 2
+  fit2 <- (-(alpha_m+mu_mc+mu_mv) - sqrt( (alpha_m+mu_mc+mu_mv)^2 - 4*(alpha_m*mu_mv + mu_mv*mu_mc - (beta_m*(gamma_m*alpha_m+lambda_m)*mu_c*(mu_v + alpha))/((gamma*alpha + lambda)*beta) ) ) ) / 2
+  return(fit1)
+}
+#******************************************************************************************
+exp(-mu_c*tau)
+
+
+mutfit.delay <- function(mu_mv=0.1            # mutant virus clearance rate
+                   ,mu_mc = 1/120       # mutant virus cell death rate
+                   ,beta_m = 10^-6      # mutant probability of infecting susceptible cells
+                   ,gamma_m = 2400      # mutant virus yeild at apoptosis
+                   ,lambda_m = 100
+                   ,f.alpha_m = 24      # fixed mutant virus time to apoptosis
+                   ,tau_m = 2           # delay in budding
+                   ,mu_v=0.1            # resident virus clearance rate  
+                   ,mu_c = 1/120        # resident infected cell death rate 
+                   ,beta = 10^-6        # resident probability of infecting susceptible cells
+                   ,f.alpha = 24          # resident virus fixed time to apoptosis
+                   ,gamma = 2400 
+                   ,lambda = 100
+                   ,tau=2){    
+  fit1 <- (-(alpha_m+mu_mc+mu_mv) + sqrt( (alpha_m+mu_mc+mu_mv)^2 - 4*(alpha_m*mu_mv + mu_mv*mu_mc - (beta_m*(gamma_m*alpha_m+lambda_m)*mu_c*(mu_v + alpha) )/((gamma*alpha + lambda)*beta)) ) ) / 2
+  rho <- exp(-mu_c*tau)
+  part.1 <- (1 / 2*(1+beta_m*Shat*f.alpha_m*rho_m*(gamma_m + tau_m*lambda_m)
+  part.2 <- (-mu_mc-mu_mv-beta_m*Shat*lambda_m*tau_m+beta_m*Shat*rho_m*(gamma_m - f.alpha_m*mu_mc*gamma_m + f.alpha_m*lambda + tau*lambda)
+  part.3 <-
+  fit1 <- 
+  return(fit1)
+}
+
+#************calculate for without delay/ fixed time*********************
+mutfit.v <- Vectorize(mutfit)         # enable multiple values to be input for a parameter
+
+lam <- seq(1,100,0.1) 
+lam.mut.a24 <- mutfit.v(lambda_m=lam,alpha_m=0,gamma_m=0,lambda=0,gamma=2400,alpha=1/24)
+mut.fitness.dat <- cbind.data.frame(lam,lam.mut.a24)
+
+
+#*****************Plot without delay/ fixed time***************************
+pdf(file="fig_ms_2.pdf",width=5,height=4)
+ggplot(mut.fitness.dat, aes(x=lam,y=lam.mut.a24)) +
+  geom_line(aes(x=lam,y=lam.mut.a24)) +
+  labs(y="Mutant virus fitness", x=expression(paste("Mutant virus budding rate (",italic(lambda)["m"],")")),")")
+dev.off()
+#*********************************************************************************************
+
+# re-write mutant virus fitness function for input to unitroot.all function
+fun <- function(x
+                ,mu_mv=0.1            # mutant virus clearance rate
+                ,mu_mc = 1/120       # mutant virus cell death rate
+                ,beta_m = 10^-6      # mutant probability of infecting susceptible cells
+                ,gamma_m = 2400      # mutant virus yeild at apoptosis
+                ,lambda_m = 100
+                ,alpha_m = 1/24      # mutant virus apoptosis rate
+                ,mu_v=0.1            # resident virus clearance rate  
+                ,mu_c = 1/120        # resident infected cell death rate 
+                ,beta = 10^-6        # resident probability of infecting susceptible cells
+                ,alpha = 1/24        # resident virus apoptosis rate
+                ,gamma = 2400
+                ,lambda = 100
+){
+  
+  if(gamma=="NA"){
+    return( ( -(0+mu_mc+mu_mv) + sqrt( (0+mu_mc+mu_mv)^2 - 4*(0*mu_mv + mu_mv*mu_mc - ( (beta_m*(0*0+lambda_m)*mu_c*(mu_v+alpha))/( (x*alpha+0)*beta) ) ) ) )  / 2 )
+  }
+}
+
+
+lambda_m <-  seq(1,100,0.1)# range of values for mutant virus probabillity of susceptible cell infection
+fit.bound1 <- sapply(lambda_m,function(y){
+  uniroot.all(fun,c(0,10^3*24),lambda_m=y,gamma="NA")
+})
+
+bounds <- cbind.data.frame(lambda_m,"fit.bound1"=fit.bound1/24)
+
+bounds[,2]/bounds[,1] # 6 *
+
+pdf(file="fig_ms_2.pdf",width=5,height=4)
+ggplot(bounds, aes(x=fit.bound1,y=lambda_m)) +
+  geom_line(aes(x=fit.bound1,y=lambda_m)) +
+  labs(y=expression(paste("Mutant virus budding rate ( ",lambda["m"],")" )), x=expression(paste("Resident virus yeild at apopotosis * apopotosis rate (",gamma, alpha,")")))
+dev.off()
+
+
+
